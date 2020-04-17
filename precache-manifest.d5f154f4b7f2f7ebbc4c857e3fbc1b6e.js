a1dc5543bed40352c57cb02811f69f472bd8025d
Esmail <elmoussel12@gmail.com>
2020-04-17 18:27:51 +0200

    Updates

---
+++ precache-manifest.d5f154f4b7f2f7ebbc4c857e3fbc1b6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f788c705c8c093b068bfd6867d84bb",
+    "url": "/Chat-App-Frontend/index.html"
+  },
+  {
+    "revision": "59dc53d565ab6a839fb9",
+    "url": "/Chat-App-Frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8413a9935bf1c5230aec",
+    "url": "/Chat-App-Frontend/static/css/main.39437af3.chunk.css"
+  },
+  {
+    "revision": "59dc53d565ab6a839fb9",
+    "url": "/Chat-App-Frontend/static/js/2.1fc33d85.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/Chat-App-Frontend/static/js/2.1fc33d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8413a9935bf1c5230aec",
+    "url": "/Chat-App-Frontend/static/js/main.4cc487e6.chunk.js"
+  },
+  {
+    "revision": "addd89c6d19d37e330d1",
+    "url": "/Chat-App-Frontend/static/js/runtime-main.432d6cee.js"
+  }
+]);
